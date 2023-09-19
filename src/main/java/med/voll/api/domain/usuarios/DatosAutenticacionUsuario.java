10cdf350a4a6c15a305fa539ecc2561d93b70de6
CamSandoval <juanprogram06@gmail.com>
2023-09-19 11:11:51 -0500

    feat(infra/security and Usuario):A new security configuration was created as stateless. A controller was created to receive requests to authenticate users. A password encryption algorithm was established

---
+++ src/main/java/med/voll/api/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.usuarios;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosAutenticacionUsuario (
+
+        @NotBlank
+        String login,
+
+        @NotBlank
+        String clave){
+}
